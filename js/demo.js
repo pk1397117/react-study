0cc8699c604d2f8eb34054cf81ed34d7bc0f8c49
Administrator <997843624@163.com>
2021-02-02 22:55:03 +0800

    第一次提交

---
+++ js/demo.js
@@ -0,0 +1,16 @@
+class ShoppingList extends React.Component {
+    render() {
+        return (
+            <div className="shopping-list">
+                <h1>Shopping List for {this.props.name}</h1>
+                <ul>
+                    <li>Instagram</li>
+                    <li>WhatsApp</li>
+                    <li>Oculus</li>
+                </ul>
+            </div>
+        );
+    }
+}
+const domContainer = document.querySelector('ShoppingList');
+ReactDOM.render(e(ShoppingList), domContainer);
